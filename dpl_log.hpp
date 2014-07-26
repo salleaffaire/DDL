13b21dba9e18a9fd32bc76a3179dc8cc6780bc8d
Luc Martel <salleaffaire@gmail.com>
2014-07-26 16:33:23 -0400

    Adding file headers with GPL license

--- dpl_log.hpp
+++ dpl_log.hpp
@@ -1,3 +1,23 @@
+//    =======================================================================
+//    DDL is the Data Driven Language and provides a simple framework
+//    to build asynchonous systems
+//    ======================================================================= 
+//    Copyright (C) 2014  Luc Martel
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//    ======================================================================= 
+
 #ifndef __DPL_LOG__
 #define __DPL_LOG__
 
